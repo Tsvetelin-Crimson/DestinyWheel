94ef0a3d773ee42352a3df26b751d585ef034ada
Tsvetelin-Crimson <67798636+Tsvetelin-Crimson@users.noreply.github.com>
2020-12-15 15:40:18 +0200

    Add files via upload
    Initial commit for JS files.

---
+++ Destiny Wheel/src/index.js
@@ -0,0 +1,11 @@
+
+const rollButton = document.getElementById('rollButton');
+
+
+rollButton.addEventListener('click', roll)
+
+function roll(e){
+    // e.preventDefault();  // if need be re-add
+
+    console.log('in event');
+}
